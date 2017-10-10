70683399a0c1a94d3e89b4576e2be7ebeea8fa81
manavortex <manavortex@gmail.com>
2017-10-10 09:45:24 +0200

    Create rubular.js
    Check https://monosnap.com/file/GbI7CKziEtVBpJJ7s3hp3Mze5vwcY9 for how it looks.

---
+++ rubular.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name        rubular
+// @namespace   rubular.com
+// @include     http://rubular.com/
+// @version     1
+// @grant       none
+// ==/UserScript==
+function addText(targetNode, text){
+  var p = document.createElement("p");
+  var textnode = document.createTextNode(text);
+  p.appendChild(textnode);
+  $(p).setAttribute('style', "margin: 0.2em; font-family: sans-serif;");
+  targetNode.appendChild(p);
+}
+function init(){
+  var $ = window.$;
+  var body = document.getElementsByTagName('body')[0];
+  var main = document.getElementById('main');
+  $(main).setAttribute('position', 'relative');
+  // A $( document ).ready() block.
+  console.log("hi");
+  var node = document.createElement("DIV");
+  addText(node, "grey(?=hound) matches grey, but only if it is followed by hound");
+  addText(node, "grey(?!hound) matches grey, but only if it is not followed by hound");
+  addText(node, "(?<=grey)hound matches hound, but only if it is preceeded by hound");
+  addText(node, "(?<!grey)hound matches hound, but only if it is not preceeded by hound");
+  var style = 'position: absolute; width: 35em; height: 10em; top: 0; left: 0; color: white; text-align: left;';
+  style = style + 'text-shadow: 3px 3px 3px #232323, -3px -3px 3px #232323, -3px 3px 3px #232323, 3px -3px 3px #232323;';
+  $(node).setAttribute('style', style );
+
+  main.appendChild(node);
+}
+
+window.setTimeout(init, 2000);
