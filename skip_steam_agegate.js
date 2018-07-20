c37339a2dc4efd50a67b57ef0d85bbacf154e8e3
manavortex <manavortex@gmail.com>
2018-07-20 09:47:18 +0200

    Update skip_steam_agegate.js

--- skip_steam_agegate.js
+++ skip_steam_agegate.js
@@ -4,7 +4,7 @@
 // @version 1.0
 // @author manavorex
 // @license MIT
-// @include *
+// @include https:\/\/store.steampowered.com\/.*agegate
 // @icon https://steamstore-a.akamaihd.net/public/shared/images/header/globalheader_logo.png?t=962016
 // @run-at document-end
 // @grant none
