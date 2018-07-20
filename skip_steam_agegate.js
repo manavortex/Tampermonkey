854f302770a3d88b4b537b6779513bd1cb2f7fdf
manavortex <manavortex@gmail.com>
2018-07-20 09:45:23 +0200

    Update skip_steam_agegate.js

--- skip_steam_agegate.js
+++ skip_steam_agegate.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name         Hide steam agegate
-// @description  Click the button that hides the steam agegate
-// @version      1.0
-// @author       manavortex
-// @match        https://store.steampowered.com/*/agecheck
-// @license      MIT
-// @run-at       document-end
-// @grant        none
+// @name Hide steam agegate
+// @description Click the button that hides the steam agegate
+// @version 1.0
+// @author manavortex
+// @match https://store.steampowered.com/*/agecheck
+// @license MIT
+// @run-at document-end
+// @grant none
+// @icon https://steamstore-a.akamaihd.net/public/shared/images/header/globalheader_logo.png?t=962016
 // ==/UserScript==
 
 (function() {
